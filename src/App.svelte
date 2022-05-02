fac97e2f7b9b467a333bbad4dbc499216fd4e781
Danny McClain <dannyxmcclain@gmail.com>
2022-05-02 14:46:30 -0500

    more cleanup

--- src/App.svelte
+++ src/App.svelte
@@ -29,12 +29,15 @@
 	h1, h2 {
 		font-weight: 700;
 	}
+
 	h1 {
 		font-size: 2rem;
 	}
+	
 	h2 {
 		font-size: 1.5rem;
 	}
+	
 	main {
 		text-align: left;
 		padding: 4em;
@@ -61,7 +64,23 @@
 
 	code {
 		font-family: inherit;
-		font-weight: 700;
+		font-weight: 600;
+		color: #3B4549;
+		padding: .125rem .25rem;
+		background: #F5F8FA;
+		border-radius: .25rem;
+	}
+
+	a {
+		font-weight: 500;
+		color: #6F3C94;
+		padding: .125rem .25rem;
+		background: #F9EDFF;
+		border-radius: .25rem;
+		
+		&:hover {
+			color: #6F3C94;
+		}
 	}
 
 	@media (min-width: 640px) {
